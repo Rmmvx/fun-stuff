60bd46db8718fe0e34445bbb274dea1022fff1cb
Romel Munoz <munoz_romel@yahoo.com>
2023-02-21 14:22:27 -0500

    Works great!

--- src/VideoStreaming.java
+++ src/VideoStreaming.java
@@ -1,24 +1,40 @@
 import java.net.*;
 import java.io.*;
-import java.awt.image.BufferedImage;
+import java.awt.image.*;
+import java.awt.FlowLayout;
 import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class VideoStreaming{
     public static void main(String[] args){
+        //Sets the frame to display video
+        JFrame nFrame = new JFrame();
+        nFrame.setLayout(new FlowLayout());
+        nFrame.setSize(1280,720);
+        JLabel lbl = new JLabel();
+
         
+        int timer = 10000;
         try{
             System.out.println("Starting");
             DatagramSocket socket = new DatagramSocket(12345);
 
-            while(true){
+            //Currently, endless loop
+            while(timer != 0){
                 byte[] buffer = new byte[65536];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                System.out.println("waiting for connection");
                 socket.receive(packet);
                 ByteArrayInputStream inStream = new ByteArrayInputStream(buffer);
                 BufferedImage newImage = ImageIO.read(inStream);
-                ImageIO.write(newImage, "jpg" , new File("output.jpg"));
-                System.out.println("image generated");
+                
+                ImageIcon icon = new ImageIcon(newImage);
+
+                lbl.setIcon(icon);
+                nFrame.add(lbl);
+
+                nFrame.setVisible(true);
+                nFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                timer--; 
             }
         }
         catch(SocketException e){
