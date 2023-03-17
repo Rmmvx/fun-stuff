66b534e475bbafb699894d14ee0c476c77910ada
Romel Munoz <munoz_romel@yahoo.com>
2023-03-17 09:33:24 -0400

    added method templates

--- src/JavaNetworking.java
+++ src/JavaNetworking.java
@@ -7,19 +7,19 @@ import java.io.*;
 
 public class JavaNetworking {
     /**
-    * Local host IP address used for testing
-    */
+     * Local host IP address used for testing
+     */
     public static final String LOCAL_HOST = "127.0.0.1";
 
     /** This method listens in a specified port
-    * if a connection can be made, it returns the socket through which
-    * communication can be made.
-    * 
-    * @param portNumber user specified port number
-    * @param password message recognized by our beaglebone server program
-    * 
-    * @return socket through which communication is possible.
-    */
+     * if a connection can be made, it returns the socket through which
+     * communication can be made.
+     * 
+     * @param portNumber user specified port number
+     * @param password message recognized by our beaglebone server program
+     * 
+     * @return socket through which communication is possible.
+     */
     public Socket createConnection(int portNumber, String hostName, String clientPass, String serverPass){
         Socket socket = null;
         BufferedReader receivedMessage;
@@ -54,6 +54,66 @@ public class JavaNetworking {
         return socket;
     }
 
+    /** This method sends commands to the beaglebone
+     * to turn left
+     * 
+     * @param socket
+     * @param command
+     * 
+     * @return 0 if success
+     */
+    public int moveLeft(Socket socket){
+        return 0;
+    }
+    
+    /** Sends comnand to beablebone to move right
+     * 
+     * @param socket
+     * @return 0 if success
+     */
+    public int moveRight(Socket socket){
+        return 0;
+    }
+
+    /** Sends command to move forwards
+     * @param socket
+     * @return 0 if success
+     */
+    public int moveForward(Socket socket){
+        return 0;
+    }
+
+    /** Sends command to either back up
+     * 
+     * @param socket
+     * @return 0 if success
+     */
+    public int moveInReverse(Socket socket){
+        return 0;
+    }
+    /** This method sends the command to start video streaming
+     * 
+     * @param socket
+     * 
+     * @return true if stream starts successfully
+     */
+    public boolean startStreaming(Socket socket){
+        return false;
+    }
+
+    /** Stops video streaming
+     *  
+     * @param socket
+     * @return true if successful
+     */
+    public boolean stopStreaming(Socket socket){
+        //If it's not streaming, simply return true
+        boolean flag = false;
+        if (!startStreaming(socket)){
+            return true;
+        }
+        return flag;
+    }
     /** Main method for debugging
      * 
      * @param args
