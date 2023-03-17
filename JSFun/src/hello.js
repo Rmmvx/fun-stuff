fcca08f9f47cb747bb27316722deb083471d7399
Romel Munoz <munoz_romel@yahoo.com>
2023-03-17 10:04:29 -0400

    added new folder

---
+++ JSFun/src/hello.js
@@ -0,0 +1,9 @@
+const mission = process.argv[2];
+
+if (mission === 'learn node.js'){
+    console.log('Time to write some code!');
+}
+
+else{
+    console.log(`Is ${mission} really more fun?`);
+}
